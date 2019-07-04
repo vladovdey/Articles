9bf7b97af210e6d018be7917f60df9e7f0e2a555
vladovdey <ovdevlad@gmail.com>
2019-07-05 01:24:47 +0300

    without favourite functionality

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -1,33 +1,44 @@
 <template>
   <v-container fluid>
-    <div > 
-      <h3>{{post.title}}</h3>
-      <p>{{post.body}}</p>
-    </div>
+
+      <v-flex tag="h1" class="headline">{{post.title}}</v-flex>
+      <v-flex>
+      <v-img
+          :src="'https://picsum.photos/id/'+post.id+'/1000/800'"
+          aspect-ratio="2.75"
+      ></v-img> </v-flex>
+      <v-flex>
+        <v-card flat>
+          <v-card-text>{{ post.body }}</v-card-text>
+        </v-card>
+      </v-flex>
+    <v-btn href="/">На главную</v-btn>
   </v-container>
 </template>
 
-<script>
-import axios from 'axios'
-import posts from '../posts.js'
 
+
+<script>
+import axios from "axios";
+import posts from "../posts.js";
 
 export default {
-  name: 'ArticlesList',
+  name: "ArticlesList",
   data() {
     return {
       posts: posts,
       post: null
-    }
+    };
   },
   created: function() {
     var postId = this.$route.params.id;
-    this.post = this.posts[postId]
+    this.post = this.posts[postId];
   }
-  
-}
+};
 </script>
 
 <style scoped>
-
+.headline{
+  text-transform: uppercase;
+}
 </style>
--- src/components/ArticlesList.vue
+++ src/components/ArticlesList.vue
@@ -1,12 +1,23 @@
 <template>
-  <div>
-    <div class="post" :key="post.id" v-for="(post,index) in paginatedData">
-      <h3>{{post.title}}</h3>
-      <!-- <img :src="'https://picsum.photos/200/300?random='.index" /> -->
-      <router-link :to="{name:'post',params:{id:index}}">Открыть</router-link>
-    </div>
-    <button v-if="(pageNumber)> 0" @click="prevPage">Предыдущая</button><span class="page">{{pageNumber+1}} страница</span><button v-if="(pageNumber+1) < pages" @click="nextPage">Следующая</button>
-  </div>
+  <v-container fluid>
+    <v-card class="post" :key="post.id" v-for="(post,index) in paginatedData">
+      <v-img
+          :src="'https://picsum.photos/id/'+post.id+'/1000/800'"
+          aspect-ratio="2.75"
+        ></v-img>
+
+      <v-card-title primary-title>
+        <h3>{{post.title}}</h3>
+      </v-card-title>
+      
+      <v-card-actions>
+        <!-- <router-link :to="{name:'post',params:{id:index}}"> -->
+          <v-btn :to="{name:'post',params:{id:index}}" flat color="blue">Подробнее</v-btn>
+          <!-- </router-link> -->
+      </v-card-actions>
+    </v-card>
+    <v-btn v-if="(pageNumber)> 0" @click="prevPage">Предыдущая</v-btn><span class="page">{{pageNumber+1}} страница</span><v-btn v-if="(pageNumber+1) < pages" @click="nextPage">Следующая</v-btn>
+  </v-container>
 </template>
 
 <script>
@@ -44,15 +55,10 @@ export default {
 </script>
 
 <style scoped>
-.post {
-  border: 1px solid black;
+ .post {
+  text-transform: uppercase;
   margin: 10px 0px;
   padding: 10px 10px;
-  width: 200px;
-}
-
-.post h3 {
-  padding-bottom: 10px;
 }
 
 .page{
